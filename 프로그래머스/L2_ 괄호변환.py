a5c60ffa84a054acf5e882ad307c20c7ce79fba0
cslee <cs.lee@kaist.ac.kr>
2022-04-18 23:14:18 +0900

    u

--- 프로그래머스/L2_ 괄호변환.py
+++ 프로그래머스/L2_ 괄호변환.py
@@ -2,7 +2,7 @@ p = "(()())()"
 # p = ")("
 # p = "()))((()"
 
-def check_string(sentence):
+def solution(sentence):
     if len(sentence) == 0:
         return ''
 
@@ -10,36 +10,45 @@ def check_string(sentence):
     cnt_right = 0
     stack = []
     answer = ''
+    token = True
 
     for i in range(len(sentence)):
-        # 균형잡힌 괄호 문자열
         if cnt_left == cnt_right and cnt_left != 0:
-            u = sentence[:cnt_left+cnt_right]
-            v = sentence[cnt_left+cnt_right:]
-
-            # 3. u가 올바른 괄호 문자열이라면 (stack이 비어있음)
-            if not stack:
-                answer = u + check_string(v)
-                return answer
-            else:
-                answer = '(' + check_string(v) + ')'
-                for alpha in u[1:-1]:
-                    if alpha == '(':
-                        answer += ')'
-                    else:
-                        answer += '('
-                return answer
-
+            return check_string(stack, cnt_left, cnt_right, token, sentence)
 
         if sentence[i] == '(':
             stack.append(sentence[i])
             cnt_left += 1
 
         if sentence[i] == ')':
-            if stack[i-1] == '(':
-                stack.pop()
+            try:
+                if stack[-1] == '(':
+                    stack.pop()
+            except:
+                token = False
 
             cnt_right += 1
 
+    # for문 밖도 동일하게 작동
+    return check_string(stack, cnt_left, cnt_right, token, sentence)
 
-print(check_string(p))
+def check_string(stack, cnt_left, cnt_right, token, sentence):
+    answer = ''
+
+    u = sentence[:cnt_left + cnt_right]
+    v = sentence[cnt_left + cnt_right:]
+
+    # 3. u가 올바른 괄호 문자열이라면 (stack이 비어있음)
+    if not stack and token == True:
+        answer = u + str(solution(v))
+        return answer
+    else:
+        answer = '(' + str(solution(v)) + ')'
+        for alpha in u[1:-1]:
+            if alpha == '(':
+                answer += ')'
+            else:
+                answer += '('
+        return answer
+
+print(solution(p))
