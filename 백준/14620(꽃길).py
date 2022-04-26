35d6c40c830b2898213ecbad7b3079307d371594
cslee <cs.lee@kaist.ac.kr>
2022-04-27 00:08:29 +0900

    u

--- 백준/14620(꽃길).py
+++ 백준/14620(꽃길).py
@@ -1,60 +1,58 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
-data_map = [list(map(int, input().split())) for _ in range(N)]    
-dx = [-1,1,0,0,0]
-dy = [0,0,-1,1,0]
 
-min_cost = 987654321 # 최소 가격 지정
-sum_list = []
-seed_list = []
-for i in range(1,N-1):
-    for j in range(1,N-1):
-        cost = 0 # 비용
-        
-        for k in range(5):
-            nx = i + dx[k]
-            ny = j + dy[k]
-            
-            cost += data_map[nx][ny]
-        
-        sum_list.append(cost)
-        seed_list.append((i,j))
-        
-# 상하좌우, 대각선, 상하좌우 2칸씩 띄기
-ban_condition_x = [0, -1,-1,-1,0,0,1,1,1, -2,0,0,2]
-ban_condition_y = [0, -1,0,1,-1,1,-1,0,1, 0,-2,2,0]
+graph = []
+for i in range(N):
+    graph.append(list(map(int, input().split())))
 
-def ban_confirm(seed_1, seed_2):
-    x = seed_1[0]
-    y = seed_1[1]
-    
-    for z in range(13):
-        nx = x + ban_condition_x[z]
-        ny = y + ban_condition_y[z]
-#         print((nx,ny))
-        if (nx,ny) == seed_2:
-            return False # 하나라도 충돌이 있으면 False
-        else:
+visited = [[False] * N for i in range(N)]
+dr = [-1, 1, 0, 0, 0]
+dc = [0, 0, -1, 1, 0]
+min_sum = 987654321
+
+# 범위 체크
+def check(r, c):
+    for i in range(5):
+        nr = r + dr[i]
+        nc = c + dc[i]
+
+        if 0 <= nr < N and 0 <= nc < N and visited[nr][nc] == False:
             continue
-            
-    return True # 모두 충돌하지 않는다면 True
+        else:
+            return False
+    return True
 
-length = len(seed_list)
-min_cost = 987654321
+# 꽃이 펼쳐지는 지역의 합
+def calculate(r, c):
+    tmp_sum = 0
+    for i in range(5):
+        nr = r + dr[i]
+        nc = c + dc[i]
+        tmp_sum += graph[nr][nc]
+    return tmp_sum
 
-for i in range(0, length-2):
-    for j in range(i+1, length-1):
-        for k in range(j+1, length):
-            # i,j,k 가 가능한 조합인지 파악
-            seed_1 = seed_list[i]
-            seed_2 = seed_list[j]
-            seed_3 = seed_list[k]
-            
-            # 모두 True 일 때, 3가지 씨앗이 가능한 경우이므로 최소 비용 저장
-            if(ban_confirm(seed_1, seed_2) and ban_confirm(seed_1, seed_3) and ban_confirm(seed_2, seed_3)):
-                cost = sum_list[i] + sum_list[j] + sum_list[k]
+def make_seed(cost, cnt):
+    global min_sum
+    if cnt == 3:
+        # 씨앗 위치의 최소합 구하기
+        min_sum = min(min_sum, cost)
+        return
+    else:
+        for r in range(1, N-1):
+            for c in range(1, N-1):
+                if check(r, c):
+                    for i in range(5):
+                        nr = r + dr[i]
+                        nc = c + dc[i]
+                        visited[nr][nc] = True
 
-                # 최소 비용 저장
-                if cost < min_cost:
-                    min_cost = cost
+                    make_seed(cost + calculate(r, c), cnt + 1)
+                    for i in range(5):
+                        nr = r + dr[i]
+                        nc = c + dc[i]
+                        visited[nr][nc] = False
 
-print(min_cost)
+
+make_seed(0, 0)
+print(min_sum)v
---
+++ 삼성_소프트웨어_아카데미/test.py
@@ -0,0 +1,59 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+stu_dict = dict()
+graph = [[0] * N for i in range(N)]
+dr = [1,-1,0,0]
+dc = [0,0,1,-1]
+answer = 0
+
+for i in range(N**2):
+    input_list = list(map(int, input().split()))
+    stu_id = input_list[0]
+    stu_like = input_list[1:]
+    stu_dict[stu_id] = stu_like
+    max_like_cnt = -1
+    max_empty_cnt = -1
+
+    for r in range(N):
+        for c in range(N):
+            if graph[r][c] == 0:
+                like_cnt = 0
+                empty_cnt = 0
+
+                for i in range(4):
+                    nr = r + dr[i]
+                    nc = c + dc[i]
+
+                    if 0 <= nr < N and 0 <= nc < N:
+                        if graph[nr][nc] in stu_like:
+                            like_cnt += 1
+
+                        if graph[nr][nc] == 0:
+                            empty_cnt += 1
+
+                if (max_like_cnt < like_cnt) or (max_like_cnt == like_cnt and max_empty_cnt < empty_cnt):
+                    max_like_cnt = like_cnt
+                    max_empty_cnt = empty_cnt
+                    max_r = r
+                    max_c = c
+
+    graph[max_r][max_c] = stu_id
+
+for r in range(N):
+    for c in range(N):
+        like_stu_list = stu_dict[graph[r][c]]
+        cnt = 0
+
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            if 0 <= nr < N and 0 <= nc < N:
+                if graph[nr][nc] in like_stu_list:
+                    cnt += 1
+
+        if cnt != 0:
+            answer += 10 ** (cnt-1)
+
+print(answer)
