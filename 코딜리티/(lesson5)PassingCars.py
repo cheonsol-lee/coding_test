8f456c3d69093f9eaf71560c3025b4ee0899496d
cslee <cs.lee@kaist.ac.kr>
2022-05-18 21:29:35 +0900

    u

---
+++ 코딜리티/(lesson5)PassingCars.py
@@ -0,0 +1,21 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    MAX = 1000000000
+    ans = 0
+    cnt_1 = 0
+    for i in reversed(A):
+        if i == 1:
+            cnt_1 += 1
+        else:
+            ans += cnt_1
+
+    if ans > MAX:
+        return -1
+    else:
+        return ans
+
+A = [0,1,0,1,1]
+print(solution(A))
+
