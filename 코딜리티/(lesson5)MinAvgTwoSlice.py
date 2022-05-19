366b606f03a17d73b96653d8594b7cd98f69731e
cslee <cs.lee@kaist.ac.kr>
2022-05-19 22:16:20 +0900

    u

--- 코딜리티/(lesson5)MinAvgTwoSlice.py
+++ 코딜리티/(lesson5)MinAvgTwoSlice.py
@@ -6,21 +6,21 @@ def solution(A):
     if N == 2: return ans
 
     for i in range(3, N+1):
+        # 슬라이스 3칸
+        avg = sum(A[i - 3:i]) / 3
+        if avg < min_val:
+            min_val = avg
+            ans = i - 3
+
         # 슬라이스 2칸
         avg = sum(A[i-2:i]) / 2
         if avg < min_val:
             min_val = avg
             ans = i-2
 
-        # 슬라이스 3칸
-        avg = sum(A[i - 3:i]) / 3
-        if avg < min_val:
-            min_val = avg
-            ans = i-3
-
     return ans
 
-A = [4, 2, 2, 5, 1, 5, 8]
+# A = [4, 2, 2, 5, 1, 5, 8]
 # A = [9,9,9]
-# A = [2,3,1]
+A = [2,3,1]
 print(solution(A))
