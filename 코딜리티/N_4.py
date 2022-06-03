f49f0b52ae6c5f6405880f3fd6ca465f80ec2d95
cslee <cs.lee@kaist.ac.kr>
2022-06-03 16:54:33 +0900

    u

--- 코딜리티/N_4.py
+++ 코딜리티/N_4.py
@@ -1,20 +1,51 @@
 def solution(A):
     cnt = 0
     max_cnt = 0
+    sign_arr = [0] * len(A) # 부호 저장
+
+    # 맨 처음에 0이 등장했을 때, 처리
+    if A[0] == 0:
+        if A[1] > 0:
+            sign_arr[0] = '-'
+        if A[1] < 0:
+            sign_arr[0] = '+'
+
+    # 부호 붙이기
+    for i in range(0, len(A)):
+        if A[i] > 0:
+            sign_arr[i] = '+'
+        if A[i] < 0:
+            sign_arr[i] = '-'
+
+        # 중간에 0이 등장했을 때, 처리
+        if A[i] == 0 and i != 0:
+            if sign_arr[i-1] =='+':
+                sign_arr[i] = '-'
+            else:
+                sign_arr[i] = '+'
+
+    # 가장 긴 배열 찾기
     for i in range(0, len(A)-1):
-        if A[i] >= 0 and A[i+1] <= 0:
+        if sign_arr[i] == '+' and sign_arr[i+1] == '-':
             cnt += 1
-            print(i, cnt)
-        elif A[i+1] >= 0 and A[i] <= 0:
+        elif sign_arr[i] == '-' and sign_arr[i+1] == '+':
             cnt += 1
-            print(i, cnt)
         else:
-            max_cnt = max(max_cnt, cnt)
             cnt = 0
 
-    return max_cnt
+        max_cnt = max(max_cnt, cnt)
+
+    print(sign_arr)
+    if max_cnt == 0:
+        return max_cnt
+    else:
+        return max_cnt+1 # (+,-) 또는 (-,+)는 항상 쌍으로 존재
 
 
 
 A = [5, 4, -3, 2, 0, 1, -1, 0, 2, -3, 4, -5]
+A = [1,0,-1]
+A = [1,1,1,1,0,1,1,-1]
+A = [-1,-1,-1,0]
+A = [-1,-1,-1]
 print(solution(A))
